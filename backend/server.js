e5603274df8c5a0abbc2786351bba2fd49f84112
claracmy <cheung.cmy@gmail.com>
2019-04-13 18:02:08 +0100

    add backend

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const express = require("express");
+var cors = require("cors");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const Data = require("./data");
+
+const API_PORT = 3001;
+const app = express();
+app.use(cors());
+const router = express.Router();
--- public/webpage.js
+++ public/webpage.js
@@ -1,1 +1,15 @@
-console.log("Hello");
+// getRestaurantDetails axios post
+// setup database // try catch
+// authentication
+
+const url = window.location.pathname.toString();
+
+function getRestaurantDetails() {
+  return {
+    restaurantID: document.querySelector(".page-header__order-info > h1")
+      .innerHTML,
+    cutlery: document.querySelector("#cutlery").value === "on" ? true : false
+  };
+}
+
+if (url.includes("checkout")) getRestaurantDetails();
