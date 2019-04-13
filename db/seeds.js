54993ce390147909336acc5af29f2192ef9c5179
claracmy <cheung.cmy@gmail.com>
2019-04-13 21:30:57 +0100

    seed data and fix schema

--- db/seeds.js
+++ db/seeds.js
@@ -1,37 +1,40 @@
-const mongoose   = require('mongoose');
-mongoose.Promise = require('bluebird');
+const mongoose = require("mongoose");
+mongoose.Promise = require("bluebird");
 
-const { dbURI } = require('../config/environment');
-const Order    = require('../model/orders');
-const User      = require('../model/users');
+const { dbURI } = require("../config/environment");
+const Order = require("../model/orders");
+const User = require("../model/users");
 
-const userData = [{
-  name: 'Sheldon',
-  lastName: 'Cooper',
-  email: 'cooper@gmail.com',
-  password: 'password',
-  passwordConfirmation: 'password',
-  currentToken: null
-}];
+const userData = [
+  {
+    name: "Sheldon",
+    lastName: "Cooper",
+    email: "cooper@gmail.com",
+    password: "password",
+    passwordConfirmation: "password",
+    currentToken: null
+  }
+];
 
 mongoose
   .connect(dbURI, { useMongoClient: true })
-  .then(db => db.dropDatabase())
   .then(() => User.create(userData))
   .then(users => {
     console.log(`${users.length} users created!`);
 
-    return Order.create([{
+    return Order.create([
+      {
         plasticUsage: {
-            cutlery: 4,
-            container: 2,
-            bag: 1,
-            bottle: 0,
-            straw:0,
-          },
-          cutlery: true,
-          additionalInfo: ""
+          cutlery: 4,
+          container: 2,
+          bag: 1,
+          bottle: 0,
+          straw: 0
         },
+        cutlery: true,
+        additionalInfo: "",
+        orderedBy: users[0]
+      }
     ]);
   })
   .then(orders => console.log(`${orders.length} orders created!`))
--- model/users.js
+++ model/users.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose");
-const orderSchema = require("./orders.js");
 
 const userSchema = new mongoose.Schema({
   name: { type: String, unique: true },
   email: { type: String, unique: true },
   numberOfOrders: { type: Number },
   vouchersAvailable: { type: Number },
-  orders: [orderSchema]
+  orders: []
 });
 
 module.exports = mongoose.model("User", userSchema);
